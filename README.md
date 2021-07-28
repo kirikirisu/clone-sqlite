4a3d4e0f71efbcdd0ee39a660713cfc9ea2dc190
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2021-07-28 17:30:04 +0900

    add sql compiler & virtual machine

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 https://cstack.github.io/db_tutorial/
 
-# getline()
+# 1. getline()
 
 - getline(char s[], int lim)
   1 行分の文字を読み取り記憶する。
@@ -10,3 +10,12 @@ https://cstack.github.io/db_tutorial/
   記憶する文字数は lim する
 - ssize_t
   変数に入る値が負の値になる場合がある時に使う
+
+# 2. prepare_statement & excute_statement
+
+- prepare_statement が sqlite のバイトコードを出力する SQL Command Processor を表現している
+- excute_statement が sqlite のバイトコードを実行する Virtural Machine を表現している
+
+prepare_statement でクエリ文をバイトコードにコンパイルする理由
+-> バイトコードをキャッシュしてパフォーマンスを向上させるため
+-> コンパイルの際に構文チェックもして、VM で構文エラーを心配しなくていいようにするため
--- sqlite.c
+++ sqlite.c
@@ -10,7 +10,33 @@ typedef struct
   ssize_t input_length;
 } InputBuffer;
 
-// インスタンスを初期化する関数
+/* メタコマンド = .から始まるコマンド */
+typedef enum
+{
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum
+{
+  PREPARE_SUCCESS,
+  PREPARE_UNRECOGNIZED_STATEMENT,
+} PrepareResult;
+
+typedef enum
+{
+  STATEMENT_INSERT,
+  STATEMENT_SELECT
+} StatementType;
+
+typedef struct
+{
+  StatementType type;
+} Statement;
+
+void close_input_buffer(InputBuffer *);
+
+/* インスタンスを初期化する関数 */
 InputBuffer *new_input_buffer()
 {
   InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
@@ -21,6 +47,54 @@ InputBuffer *new_input_buffer()
   return input_buffer;
 }
 
+MetaCommandResult do_meta_command(InputBuffer *input_buffer)
+{
+  if (strcmp(input_buffer->buffer, ".exit") == 0)
+  {
+    close_input_buffer(input_buffer);
+    exit(EXIT_SUCCESS);
+  }
+  else
+  {
+    // 1を返す
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+/* readme 2 */
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
+{
+  if (strncmp(input_buffer->buffer, "insert", 6) == 0)
+  {
+    statement->type = STATEMENT_INSERT;
+    return PREPARE_SUCCESS;
+  }
+  else
+  {
+    if (strcmp(input_buffer->buffer, "select") == 0)
+    {
+      statement->type = STATEMENT_SELECT;
+      return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+  }
+}
+
+/* readme 2 */
+void execute_statement(Statement *statement)
+{
+  switch (statement->type)
+  {
+  case (STATEMENT_INSERT):
+    printf("this is where we would do an insert. \n");
+    break;
+  case (STATEMENT_SELECT):
+    printf("this is where we would do a select. \n");
+    break;
+  }
+}
+
 void print_prompt()
 {
   printf("db > ");
@@ -28,8 +102,9 @@ void print_prompt()
 
 void read_input(InputBuffer *input_buffer)
 {
+  /* readme 1 */
   ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
-  printf("%zd\n", bytes_read);
+  // printf("%zd\n", bytes_read);
 
   if (bytes_read <= 0)
   {
@@ -57,14 +132,31 @@ int main(int argc, char *argv[])
     print_prompt();
     read_input(input_buffer);
 
-    if (strcmp(input_buffer->buffer, ".exit") == 0)
+    if (input_buffer->buffer[0] == '.')
     {
-      close_input_buffer(input_buffer);
-      exit(EXIT_SUCCESS);
+      switch (do_meta_command(input_buffer))
+      {
+      case (META_COMMAND_SUCCESS):
+        /* while文の残りの処理をスキップして、whileの最初に戻る */
+        continue;
+      case (META_COMMAND_UNRECOGNIZED_COMMAND):
+        printf("Unrecognized command '%s'\n", input_buffer->buffer);
+        continue;
+      }
     }
-    else
+
+    Statement statement;
+    switch (prepare_statement(input_buffer, &statement))
     {
-      printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+    case (PREPARE_SUCCESS):
+      /* switchを抜けて、while文の残りの処理を実行する */
+      break;
+    case (PREPARE_UNRECOGNIZED_STATEMENT):
+      printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+      continue;
     }
+
+    execute_statement(&statement);
+    printf("Executed.\n");
   }
 }
