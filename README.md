c09f1f384f68ca21c46da97d48e71adb8b176140
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2021-07-29 23:10:58 +0900

    add In-Mumory

--- README.md
+++ README.md
@@ -17,5 +17,52 @@ https://cstack.github.io/db_tutorial/
 - excute_statement が sqlite のバイトコードを実行する Virtural Machine を表現している
 
 prepare_statement でクエリ文をバイトコードにコンパイルする理由
+
 -> バイトコードをキャッシュしてパフォーマンスを向上させるため
+
 -> コンパイルの際に構文チェックもして、VM で構文エラーを心配しなくていいようにするため
+
+# 3. 一つの row の中身はメモリ上で連続して並べておく
+
+- uint32_t == unsigned int
+- Row のメンバ（attribute）のサイズを算出し、オフセットを決めることでシリアライズ（直列化）した時に
+  どこからどこまでがどのメンバの値なのかわかるようにしている。
+
+  - この場合、id = 4byte, username = 32byte, email 255byte になるため、
+    メモリ上の配列の 0~3byte 番目は id、4~35byte 番目は username、36~290byte 番目は email が入っていることがわかるようにしている。
+    <br>配列のサイズは 291byte になる
+
+- memcpy
+  - void *memcpy(void *buf1, const void \*buf2, size_t n);
+    <br>buf2 の先頭から n 文字分を buf1 にコピーする。
+
+# 4. 構造体でテーブルを表現する
+
+- page? page size?
+  paging というメモリ管理の方法がある。
+
+http://www.cs.gunma-u.ac.jp/~nakano/OS16/vm.html
+https://www.computerhope.com/jargon/p/paging.htm
+
+- void\*について
+  　<br> 単にアドレスを格納する型。どんな型の変数のアドレスでも格納できる。汎用ポインタと呼ばれる。
+
+- new_table()
+  <br>page を 100 個用意している。pages の 0 番目から 99 番目まで NULL が入る。NULL にはのちに void*が入る。void*は malloc により返された値が入る。
+
+- page の管理
+
+  - ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE = 4096 / 291 = 14.07
+
+    <br>1 つの page に 14 個 row を格納できる事になる。page は 100 個用意してあるため、合計 140 個のデータをメモリ上に保存できる。コード上では、構造体 Table の pages に row を追加していく。
+
+  - row_slot()
+    <br> row_num / POWS_PER_PAGE で row を格納する pages 配列の番号を割り出す。row がすでに 14 個格納されていれば、pages 配列の 0 番目の page は埋まっていることになる。埋まっていたら新しく PAGE_SIZE 分だけメモリを確保し、あららしく確保した領域の先頭のアドレスを pages のそれぞれの page に割り当てている。
+
+    <br> row_num % ROWS_PER_PAGE は 0 ~ 13 を繰り返す。page の中にすでに何個 row が入っているかを表す。
+
+    <br> byte_offset は単に page の何バイト目まで row が入っているかを表す。
+
+    <br> page は malloc により作られている。そのため、page には 新しく確保された領域の先頭アドレスだけ入っている。つまりこの関数は、確保した領域の何バイト目から row を追加するのかを計算している。
+
+    <br> まとめると、row が page に 14 個追加されるごとに新しい page を一つ作っている。新しい page が作られるということは、新しく作られた page は今まで作られた page とメモリ上で連続していない事になる。それぞれの page の先頭アドレスは、malloc により返された値となっていて、pages 配列のそれぞれの要素に格納されていく。
--- sqlite.c
+++ sqlite.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 typedef struct
 {
@@ -9,6 +10,12 @@ typedef struct
   size_t buffer_length;
   ssize_t input_length;
 } InputBuffer;
+
+typedef enum
+{
+  EXECUTE_SUCCESS,
+  EXECUTE_TABLE_FULL
+} ExecuteResult;
 
 /* メタコマンド = .から始まるコマンド */
 typedef enum
@@ -20,7 +27,8 @@ typedef enum
 typedef enum
 {
   PREPARE_SUCCESS,
-  PREPARE_UNRECOGNIZED_STATEMENT,
+  PREPARE_SYNTAX_ERROR,
+  PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
 typedef enum
@@ -29,12 +37,100 @@ typedef enum
   STATEMENT_SELECT
 } StatementType;
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+typedef struct
+{
+  uint32_t id;
+  char username[COLUMN_USERNAME_SIZE];
+  char email[COLUMN_EMAIL_SIZE];
+} Row;
+
 typedef struct
 {
   StatementType type;
+  Row row_to_insert;
 } Statement;
 
 void close_input_buffer(InputBuffer *);
+
+/* 与えられた構造体のメンバの大きさを返すだけ */
+#define size_of_attribte(Struct, Attribute) sizeof(((Struct *)0)->Attribute)
+
+/* readme 3 */
+const uint32_t ID_SIZE = size_of_attribte(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribte(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribte(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+/* readme 4 */
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct
+{
+  uint32_t num_rows;
+  void *pages[TABLE_MAX_PAGES];
+} Table;
+
+void print_row(Row *row)
+{
+  printf("(%d, %s, %s)\n", row->id, row->username, row->email);
+}
+
+void serialize_row(Row *source, void *destination)
+{
+  memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+  memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_OFFSET);
+  memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void *source, Row *destination)
+{
+  memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+  memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+  memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+void *row_slot(Table *table, uint32_t row_num)
+{
+  uint32_t page_num = row_num / ROWS_PER_PAGE;
+  void *page = table->pages[page_num];
+  if (page == NULL)
+  {
+    page = table->pages[page_num] = malloc(PAGE_SIZE);
+  }
+  uint32_t row_offset = row_num % ROWS_PER_PAGE;
+  uint32_t byte_offset = row_offset * ROW_SIZE;
+  printf("what this?\n%d\n", *(int *)page);
+  printf("byte_offset\n%d\n", byte_offset);
+  return page + byte_offset;
+}
+
+Table *new_table()
+{
+  Table *table = malloc(sizeof(Table));
+  table->num_rows = 0;
+  for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++)
+  {
+    table->pages[i] = NULL;
+  }
+  return table;
+}
+
+void free_table(Table *table)
+{
+  for (int i = 0; table->pages[i]; i++)
+  {
+    free(table->pages[i]);
+  }
+  free(table);
+}
 
 /* インスタンスを初期化する関数 */
 InputBuffer *new_input_buffer()
@@ -47,11 +143,12 @@ InputBuffer *new_input_buffer()
   return input_buffer;
 }
 
-MetaCommandResult do_meta_command(InputBuffer *input_buffer)
+MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table)
 {
   if (strcmp(input_buffer->buffer, ".exit") == 0)
   {
     close_input_buffer(input_buffer);
+    free_table(table);
     exit(EXIT_SUCCESS);
   }
   else
@@ -67,31 +164,57 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
   if (strncmp(input_buffer->buffer, "insert", 6) == 0)
   {
     statement->type = STATEMENT_INSERT;
+    int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
+    if (args_assigned < 3)
+    {
+      return PREPARE_SYNTAX_ERROR;
+    }
     return PREPARE_SUCCESS;
   }
-  else
+  if (strcmp(input_buffer->buffer, "select") == 0)
   {
-    if (strcmp(input_buffer->buffer, "select") == 0)
-    {
-      statement->type = STATEMENT_SELECT;
-      return PREPARE_SUCCESS;
-    }
-
-    return PREPARE_UNRECOGNIZED_STATEMENT;
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
   }
+
+  return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+ExecuteResult execute_insert(Statement *statement, Table *table)
+{
+  if (table->num_rows >= TABLE_MAX_ROWS)
+  {
+    return EXECUTE_TABLE_FULL;
+  }
+
+  Row *row_to_insert = &(statement->row_to_insert);
+
+  serialize_row(row_to_insert, row_slot(table, table->num_rows));
+  table->num_rows += 1;
+
+  return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_select(Statement *statement, Table *table)
+{
+  Row row;
+  for (uint32_t i = 0; i < table->num_rows; i++)
+  {
+    deserialize_row(row_slot(table, i), &row);
+    print_row(&row);
+  }
+  return EXECUTE_SUCCESS;
 }
 
 /* readme 2 */
-void execute_statement(Statement *statement)
+ExecuteResult execute_statement(Statement *statement, Table *table)
 {
   switch (statement->type)
   {
   case (STATEMENT_INSERT):
-    printf("this is where we would do an insert. \n");
-    break;
+    return execute_insert(statement, table);
   case (STATEMENT_SELECT):
-    printf("this is where we would do a select. \n");
-    break;
+    return execute_select(statement, table);
   }
 }
 
@@ -126,6 +249,7 @@ void close_input_buffer(InputBuffer *input_buffer)
 
 int main(int argc, char *argv[])
 {
+  Table *table = new_table();
   InputBuffer *input_buffer = new_input_buffer();
   while (true)
   {
@@ -134,7 +258,7 @@ int main(int argc, char *argv[])
 
     if (input_buffer->buffer[0] == '.')
     {
-      switch (do_meta_command(input_buffer))
+      switch (do_meta_command(input_buffer, table))
       {
       case (META_COMMAND_SUCCESS):
         /* while文の残りの処理をスキップして、whileの最初に戻る */
@@ -151,12 +275,22 @@ int main(int argc, char *argv[])
     case (PREPARE_SUCCESS):
       /* switchを抜けて、while文の残りの処理を実行する */
       break;
+    case (PREPARE_SYNTAX_ERROR):
+      printf("Syntax error. Could not parse statement.\n");
+      continue;
     case (PREPARE_UNRECOGNIZED_STATEMENT):
       printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
       continue;
     }
 
-    execute_statement(&statement);
-    printf("Executed.\n");
+    switch (execute_statement(&statement, table))
+    {
+    case (EXECUTE_SUCCESS):
+      printf("Executed. \n");
+      break;
+    case (EXECUTE_TABLE_FULL):
+      printf("Error: Table full.\n");
+      break;
+    }
   }
 }
