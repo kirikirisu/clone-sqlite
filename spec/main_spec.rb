0e7e231ecc024ee24656ec1d24b55b49f1d98416
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2021-07-30 10:31:34 +0900

    add rspec test

---
+++ spec/main_spec.rb
@@ -0,0 +1,29 @@
+describe 'database' do
+  def run_script(commands)
+    raw_output = nil
+    IO.popen("./sqlite", "r+") do |pipe| 
+      commands.each do |command|
+        pipe.puts command
+      end
+
+      pipe.close_write
+
+      raw_output = pipe.gets(nil)
+    end
+    raw_output.split("\n")
+  end
+
+  it 'inserts and retrieves a row' do 
+    result = run_script([
+      "insert 1 kkkkkkk person1@example.com",
+      "select",
+      ".exit",
+    ])
+    expect(result).to match_array([
+      "db > Executed.",
+      "db > (1, kkkkkkk, person1@example.com)",
+      "Executed.",
+      "db > ",
+    ])
+  end
+end
