24c46a00c6826c652a43ac7dc53b2e87614c8c0f
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2021-08-02 15:35:49 +0900

    add cursor

--- sqlite.c
+++ sqlite.c
@@ -90,6 +90,13 @@ typedef struct
   Pager *pager;
 } Table;
 
+typedef struct
+{
+  Table *table;
+  uint32_t row_num;
+  bool end_of_table;
+} Cursor;
+
 void print_row(Row *row)
 {
   printf("(%d, %s, %s)\n", row->id, row->username, row->email);
@@ -146,7 +153,7 @@ void *get_page(Pager *pager, uint32_t page_num)
   return pager->pages[page_num];
 }
 
-/* readme 4 */
+/* readme 4 
 void *row_slot(Table *table, uint32_t row_num)
 {
   uint32_t page_num = row_num / ROWS_PER_PAGE;
@@ -154,6 +161,46 @@ void *row_slot(Table *table, uint32_t row_num)
   uint32_t row_offset = row_num % ROWS_PER_PAGE;
   uint32_t byte_offset = row_offset * ROW_SIZE;
   return page + byte_offset;
+}
+*/
+
+Cursor *table_start(Table *table)
+{
+  Cursor *cursor = malloc(sizeof(Cursor));
+  cursor->table = table;
+  cursor->row_num = 0;
+  cursor->end_of_table = (table->num_rows == 0);
+
+  return cursor;
+}
+
+Cursor *table_end(Table *table)
+{
+  Cursor *cursor = malloc(sizeof(Cursor));
+  cursor->table = table;
+  cursor->row_num = table->num_rows;
+  cursor->end_of_table = true;
+
+  return cursor;
+}
+
+void *cursor_value(Cursor *cursor)
+{
+  uint32_t row_num = cursor->row_num;
+  uint32_t page_num = row_num / ROWS_PER_PAGE;
+  void *page = get_page(cursor->table->pager, page_num);
+  uint32_t row_offset = row_num % ROWS_PER_PAGE;
+  uint32_t byte_offset = row_offset * ROW_SIZE;
+  return page + byte_offset;
+}
+
+void cursor_advance(Cursor *cursor)
+{
+  cursor->row_num += 1;
+  if (cursor->row_num >= cursor->table->num_rows)
+  {
+    cursor->end_of_table = true;
+  }
 }
 
 /* readme 5 */
@@ -349,8 +396,9 @@ ExecuteResult execute_insert(Statement *statement, Table *table)
   }
 
   Row *row_to_insert = &(statement->row_to_insert);
+  Cursor *cursor = table_end(table);
 
-  serialize_row(row_to_insert, row_slot(table, table->num_rows));
+  serialize_row(row_to_insert, cursor_value(cursor));
   table->num_rows += 1;
 
   return EXECUTE_SUCCESS;
@@ -358,12 +406,18 @@ ExecuteResult execute_insert(Statement *statement, Table *table)
 
 ExecuteResult execute_select(Statement *statement, Table *table)
 {
+  Cursor *cursor = table_start(table);
+
   Row row;
-  for (uint32_t i = 0; i < table->num_rows; i++)
+  while (!(cursor->end_of_table))
   {
-    deserialize_row(row_slot(table, i), &row);
+    deserialize_row(cursor_value(cursor), &row);
     print_row(&row);
+    cursor_advance(cursor);
   }
+
+  free(cursor);
+
   return EXECUTE_SUCCESS;
 }
 
